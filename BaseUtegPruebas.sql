2fe0b52b46de9e4f9b9ea46e09eaa1a1a402a486
ivanbelmont <potara.cotosta@gmail.com>
2016-03-30 12:04:31 -0600

    BaseUteg

---
+++ BaseUtegPruebas.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v8.61 
+MySQL - 5.5.5-10.1.10-MariaDB : Database - pruebasuteg
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`pruebasuteg` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `pruebasuteg`;
+
+/*Table structure for table `graficas` */
+
+DROP TABLE IF EXISTS `graficas`;
+
+CREATE TABLE `graficas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) DEFAULT NULL,
+  `puntos` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+/*Data for the table `graficas` */
+
+insert  into `graficas`(`id`,`nombre`,`puntos`) values (1,'Ivan',10),(2,'Gera',8),(3,'Fer',5),(4,'Isma',9),(5,'Chuy',7);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
